2a7d0ad25fa61391f6bc5ae0783fc757543201d8
tzing @313 <tzingshih@gmail.com>
2017-04-13 00:23:20 +0800

    add crawler for index page

---
+++ src/base.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Apr 12 23:22:46 2017
+
+@author: tzu-ting
+"""
+
+class base_class:
+
+    def get_key(self) -> list:
+        pass
+
+    def get_detail(self) -> list:
+        pass
